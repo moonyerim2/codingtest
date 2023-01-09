f8883da73c9bbf339c42e3312b3ad1f137b89cf7
moonyerim2 <moonyerim2@gmail.com>
2023-01-09 14:45:23 +0900

    프로그래머스 - 올바른 괄호

--- Programmers/right-bracket.js
+++ Programmers/right-bracket.js
@@ -1,3 +1,4 @@
+//전에 푼거
 function solution(s) {
   const stack = [];
 
@@ -9,6 +10,23 @@ function solution(s) {
       stack.pop();
     }
   }
+
+  return stack.length === 0;
+}
+
+//다시 풀었음
+function solution(s) {
+  if (s[0] === ")") return false;
+  const sArr = s.split("");
+  const stack = [];
+
+  sArr.forEach((el) => {
+    if (el === "(") {
+      stack.push(el);
+    } else if (el === ")") {
+      stack.pop();
+    }
+  });
 
   return stack.length === 0;
 }
